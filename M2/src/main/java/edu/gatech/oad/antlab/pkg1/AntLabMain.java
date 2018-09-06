38897acf371cdb54401b589101f4ba565b30c439
Yasmin Martins <ymartins3@gatech.edu>
2018-09-06 17:53:36 -0400

    Added person 5

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -61,7 +61,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p1.toString("idismukes3");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("kory");
+		  Person2 p2 = new Person2("Kory");
 		  toPrint += p2.toString("kbrantley8");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Yasmin");
+                  toPrint += p5.toString("ymartins3");
 
         System.out.println(toPrint);
 
