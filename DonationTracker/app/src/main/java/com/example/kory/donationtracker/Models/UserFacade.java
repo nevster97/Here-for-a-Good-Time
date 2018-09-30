d1734c9cad4e54a4a3fc0fa53a5ede4457a1e8f6
Isaac Dismukes <idismukes3@gatech.edu>
2018-09-30 19:23:14 -0400

    test

---
+++ DonationTracker/app/src/main/java/com/example/kory/donationtracker/Models/UserFacade.java
@@ -0,0 +1,60 @@
+package com.example.kory.donationtracker.Models;
+
+import android.util.Log;
+
+// << interface >>
+public class UserFacade {
+
+    /**
+     * This is the only instance of UserFacade EVER!!!!
+     */
+    private static UserFacade INSTANCE = new UserFacade();
+    private UserManager manager;
+    private User currentUser;
+
+    public static UserFacade getInstance() { return INSTANCE; }
+
+    /**
+     * Constructs the UserFacade
+     */
+    public UserFacade() {
+        manager = new UserManager();
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public boolean hasLoggedInUser() {
+        return currentUser != null;
+    }
+
+    public boolean register(String user, String pass, String name, String contact, UserType type) {
+        if (hasLoggedInUser()) {
+            return false;
+        }
+
+        return manager.addUser(user, pass, name, contact, type);
+    }
+
+    public boolean login(String username, String password) {
+        if (hasLoggedInUser()) {
+            return false;
+        }
+
+        User user = manager.login(username, password);
+        if (user != null) {
+            currentUser = user;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void logout() {
+        currentUser = null;
+    }
+
+
+
+}
