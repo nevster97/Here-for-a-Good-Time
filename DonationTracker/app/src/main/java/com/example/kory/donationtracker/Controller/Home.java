79780af8b1349c444ab957366d2aeb2a25ce7b16
Isaac Dismukes <idismukes3@gatech.edu>
2018-10-17 14:51:14 -0400

    imported location classes to home class

--- DonationTracker/app/src/main/java/com/example/kory/donationtracker/Controller/Home.java
+++ DonationTracker/app/src/main/java/com/example/kory/donationtracker/Controller/Home.java
@@ -23,12 +23,16 @@ import com.example.kory.donationtracker.Models.UserClasses.UserFacade;
 import com.example.kory.donationtracker.Models.UserClasses.UserType;
 import com.example.kory.donationtracker.R;
 
+// importing LocationClasses package
+import com.example.kory.donationtracker.Models.LocationClasses.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Home extends AppCompatActivity implements OnItemSelectedListener {
 
@@ -230,8 +234,10 @@ public class Home extends AppCompatActivity implements OnItemSelectedListener {
                 line = br.readLine();
                 tokens = line.split(",");
             }
+
             setContentView(R.layout.activity_select_location);
             loadMenus();
+
             TextView tv = findViewById(R.id.name);
             tv.setText(tokens[1]);
             TextView tv1 = findViewById(R.id.latitude);
